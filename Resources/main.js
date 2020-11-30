5abec984f488a5e65d23676e5436e0668f0a5dfe
Luisa Venancio <luvenan@gmail.com>
2020-11-30 22:23:01 +0200

    Finished csv solution, working on rest of code

--- Resources/main.js
+++ Resources/main.js
@@ -5,25 +5,84 @@
 function makeArr() {
     let ingredients = document.getElementById('textbox').value;
     let ingredlower = ingredients.toLowerCase();
-    let arrIngred1 = ingredlower.split(', ');
-    return arrIngred1;
+    let arrIngred = ingredlower.split(', ');
+    return arrIngred;
 };
 
-//Short trigger list to test functionality
-let triggerList = ['peanut', 'almond', 'msg', 'orange', 'soy sauce', 'banana', 'chocolate'];
+
+//This reads a csv file in javascript uses it to populate an array that will be part of the loop that will provide different answers, according to flowchart I will build. 
+
+let plotcsv = document.getElementById('plotcsv')
+let btn_upload = document.getElementById('btn-upload-csv').addEventListener('click', ()=> {
+  console.log('clicked')
+  Papa.parse(document.getElementById('upload-csv').files[0], {
+    download: true,
+    header: true,
+    complete: function(csvfile){
+     // console.log(csvfile)
+     // console.log(csvfile.data[0].MSG);
+      
+      // Selects the property general triggers in each array and makes another array with only the general triggers items
+      let generalArray = [];
+      for (let i=0; i<csvfile.data.length; i++) {
+          generalArray.push(csvfile.data[i].General)
+      };
+      console.log(generalArray)
+      
+      //Selects the property msg in each array and makes another array with only the msg items
+      let msgArray = [];
+      for (let i=0; i<csvfile.data.length; i++) {
+          msgArray.push(csvfile.data[i].MSG)
+      };
+      console.log(msgArray)
+         
+      // Selects the property iffy ingredients in each array and makes another array with only the general triggers items
+      let iffyArray = [];
+      for (let i=0; i<csvfile.data.length; i++) {
+        iffyArray.push(csvfile.data[i].Iffy)
+       };
+       console.log(iffyArray)
+    
+    plotcsv.innerHTML = 'General triggers: ' + generalArray + ';' + ' Msg: ' + msgArray +';' + ' Iffy: ' + iffyArray + ';'
+  }
+    })
+  });
+
+
+
+/*let csvresult = Papa.parse('./Resources/test.csv', {
+    download: true,
+    header: true,
+    complete: function(csvfile){
+      console.log(csvfile)
+    }
+  });
+  */
+
+
+
 
 
 //This function checks for triggers. It loop through both arrays, find common items, create third array, check if any triggers. If none, say it is safe. If some, return message saying it is unsafe with trigger list.
 
-function findTriggers(arr1, arr2) {
+//Short trigger list to test functionality
+
+
+function findTriggers(arrIngred, generalArray, msgArray, iffyArray) {
+    let noTriggerCounter = 3;
+    
+    //Sample loops
+    /*
+    let triggerList = ['peanut', 'almond', 'msg', 'orange', 'soy sauce', 'banana', 'chocolate'];
     let foundTriggers = [];
-    for (let i = 0; i < arr1.length; i++) {
+        for (let i = 0; i < arr1.length; i++) {
         for (let j = 0; j < arr2.length; j++) {
             if (arr1[i] === arr2[j]) {
                 foundTriggers.push(arr1[i]);
             };
         };
     };
+
     if (foundTriggers.length === 0) {
         document.getElementById('safe').hidden = false;
         document.getElementById('notsafe').hidden = true;
@@ -35,7 +94,30 @@ function findTriggers(arr1, arr2) {
         console.log('this product has these triggers: ' + stringTriggers);
         document.getElementById('listoftriggers').innerHTML = stringTriggers;
     }
-    return foundTriggers;
+    */
+
+  //If statement for general triggers
+    let generalTriggers = [];
+    for (let i = 0; i < arrIngred.length; i++) {
+        for (let j = 0; j < generalArray.length; j++) {
+            if (arr1[i] === arr2[j]) {
+                generalTriggers.push(arrIngred[i]);
+            };
+        };
+    };
+
+    if (generalTriggers.length > 0) {
+        document.getElementById('safe').hidden = true;
+        document.getElementById('notsafe').hidden = false;
+        
+        //Edit on the HTML this part
+        let stringGen = generalTriggers.join(', ')
+        document.getElementById('gentriggers').innerHTML = stringGen;
+    } else {
+        noTriggerCounter -= 1;
+        console.log(noTriggerCounter)
+    }
+
 };
 
 
@@ -45,7 +127,7 @@ const check = document.getElementById('checkbutton');
 
 check.onclick = function() {
     console.log('ingredients have been submitted')
-    findTriggers(makeArr(), triggerList);
+    findTriggers(makeArr(), generalArray, msgArray, iffyArray);
     check.hidden = true;
     reset.hidden = false;
 }
@@ -62,67 +144,4 @@ reset.onclick = function() {
     reset.hidden = true;
     document.getElementById('safe').hidden = true;
     document.getElementById('notsafe').hidden = true;
-
 }
-
-
-
-//This reads a csv file in javascript, turns it into an array or uses it to populate an array that will be part of the loop that will provide different answers, according to flowchart I will build. Step 1 - figure out how to import Csv here, and how to access different columns in it. Or excel sheets. Or something else. To be determined. 
-//Is this now up to date?
-let plotcsv = document.getElementById('plotcsv')
-let btn_upload = document.getElementById('btn-upload-csv').addEventListener('click', ()=> {
-  console.log('clicked')
-  Papa.parse(document.getElementById('upload-csv').files[0], {
-    download: true,
-    header: true,
-    complete: function(csvfile){
-      console.log(csvfile)
-      console.log(csvfile.data[0].msg);
-      
-      //Selects the property msg in each array and makes another array with only the msg items
-      let msgarray = [];
-      for (let i=0; i<csvfile.data.length; i++) {
-          msgarray.push(csvfile.data[i].msg)
-      };
-      console.log(msgarray)
-
-      // Selects the property general triggers in each array and makes another array with only the general triggers items
-      let generalarray = [];
-      for (let i=0; i<csvfile.data.length; i++) {
-          generalarray.push(csvfile.data[i].generaltriggers)
-      };
-      console.log(generalarray)
-      
-      plotcsv.innerHTML = 'msg: ' + msgarray +';' + 'general triggers: ' + generalarray + ';'
-    }
-      })
-    });
-
-
-/*let csvresult = Papa.parse('./Resources/test.csv', {
-    download: true,
-    header: true,
-    complete: function(csvfile){
-      console.log(csvfile)
-    }
-  });
-  */
-
-//Plots the csv file parsed
-/*
-let plotcsv = document.getElementById('plotcsv')
-plotcsv.innerHTML = csvresult;
-*/
-
-//For later, trigger area, divided by categories, may change 
-/*
-let msg = [];
-let processedMeats = [];
-let dairy = [];
-let nuts = [];
-let fruits = [];
-let vegetables = [];
-let freshYeast = [];
-let sugarSubs = [];
-let others = [];
-*/
